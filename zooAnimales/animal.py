78a8591951a17737e366622318ba732e0e1642ad
AlPerBara <alperezba@unal.edu.co>
2024-12-11 13:14:10 -0500

    Add files via upload

---
+++ zooAnimales/animal.py
@@ -0,0 +1,67 @@
+from zooAnimales.anfibio import Anfibio
+from zooAnimales.ave import Ave
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.pez import Pez
+from zooAnimales.reptil import Reptil
+
+
+class Animal:
+    totalAnimales=0
+
+    def __init__(self, nombre="", edad=0, habitat="", genero="", zona=None):
+        self._nombre=nombre
+        self._edad=edad
+        self._habitat=habitat
+        self._genero=genero
+        self._zona=zona
+        Animal.totalAnimales+=1
+
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self, nombreNuevo):
+        self._nombre=nombreNuevo
+
+    def getEdad(self):
+        return self._edad
+    
+    def setEdad(self, edadNueva):
+        self._edad=edadNueva
+
+    def getHabitat(self):
+        return self._habitat
+    
+    def setHabitat(self, habitatNueva):
+        self._habitat=habitatNueva
+
+    def getGenero(self):
+        return self._genero
+    
+    def setGenero(self, generoNuevo):
+        self._genero=generoNuevo
+
+    def getZona(self):
+        return self._zona
+    
+    def setZona(self, zonaNueva):
+        self._zona=zonaNueva
+
+
+    
+    @staticmethod
+    def totalPorTipo():
+        return f"Mamíferos: {Mamifero.cantidadMamiferos()}\n" \
+               f"Aves: {Ave.cantidadAves()}\n" \
+               f"Reptiles: {Reptil.cantidadReptiles()}\n" \
+               f"Peces: {Pez.cantidadPeces()}\n" \
+               f"Anfibios: {Anfibio.cantidadAnfibios()}"
+    
+    def movimiento(self):
+        return "desplazarse"
+    
+    def toString(self):
+        if self._zona:
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona._nombre}, en el {self._zona._zoo._nombre}."
+        
+        else:
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
---
+++ zooAnimales/pez.py
@@ -0,0 +1,44 @@
+from zooAnimales.animal import Animal
+
+
+class Pez(Animal):
+    listado=[]
+    salmones=0
+    bacalaos=0
+
+    def __init__(self, nombre="", edad=0, habitat="", genero="", zona=None, colorEscamas="", cantidadAletas=0):
+        super().__init__(nombre, edad, habitat, genero, zona)
+        self._colorEscamas=colorEscamas
+        self._cantidadAletas=cantidadAletas
+        Pez.listado.append(self)
+
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setColorEscamas(self, escamas):
+        self._colorEscamas=escamas
+
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+    
+    def setCantidadAletas(self, aletas):
+        self._cantidadAletas=aletas
+
+    
+
+    @staticmethod
+    def crearSalmon(nombre, edad, genero, zona=None):
+        Pez.salmones+=1
+        return Pez(nombre, edad, "oceano", genero, zona, "rojo", 6)
+
+    @staticmethod
+    def crearBacalao(nombre, edad, genero, zona=None):
+        Pez.bacalaos+=1
+        return Pez(nombre, edad, "oceano", genero, zona, "gris", 6)
+    
+    @staticmethod
+    def cantidadPeces():
+        return len(Pez.listado)
+    
+    def movimiento(self):
+        return "nadar"
